37f41f08972be0fd1de032c46f4cdbd66babb812
adenike rj <oyeniyi229@gmai.com>
2024-11-21 15:03:18 +0100

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB without deprecated options
+// Connect to MongoDB 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
